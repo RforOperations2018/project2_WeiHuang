45d7d27af35433f7b8f5ada04a03191ca1147005
Wei Huang <whuang2@andrew.cmu.edu>
2018-10-17 22:14:48 -0400

    Commit3 - failed to pull out line data from a web's API
    fixed other problems. Failed to pull out line data from a web's API

--- Project2_WeiHuang/app.R
+++ Project2_WeiHuang/app.R
@@ -11,21 +11,9 @@ library(leaflet.extras)
 library(shinythemes)
 library(plotly)
 
-# Function to pull ESRI data
-getEsri <- function(url) {
-  # Make Call
-  g <- GET(URLencode(url))
-  c <- content(g)
-  readOGR(c)
-}
+pal5 <- colorFactor(palette = c( "#E41A1C","#377EB8","#4DAF4A","#984EA3","#FF7F00","#FFFF33"), 
+                    domain = interM$type)
 
-getEsriList <- function(url) {
-  # Make Call
-  g <- GET(URLencode(url))
-  fromJSON(content(g))$features %>% 
-    unlist() %>% 
-    unname()
-}
 
 ckanSQL <- function(url) {
   # Make the Request
@@ -37,41 +25,32 @@ ckanSQL <- function(url) {
   # Create Dataframe
   data.frame(jsonlite::fromJSON(json)$result$records)
 }
-#load the polygons, lines and points for the map
-# url_1 <- URLencode("https%3A%2F%2Fdata.wprdc.org%2Fdataset%2F37e7a776-c98b-4e08-ad61-a8c8e23ec9ab%2Fresource%2F12d59d62-e86d-4f37-af19-463050496ed6%2Fdownload%2Fplaygrounds_img.geojson")
-# cities <- getEsriList(url_1)
 
-
-# pdf(NULL)
+pdf(NULL)
 # 
- # Unique values for Resource Field
- # ckanUniques <- function(id, field) {
- #   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20DISTINCT(%22", field, "%22)%20from%20%22", id, "%22")
- #   c(ckanSQL(URLencode(url)))
- # }
+# Unique values for Resource Field
+  ckanUniques <- function(id, field) {
+    url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20DISTINCT(%22", field, "%22)%20from%20%22", id, "%22")
+    c(ckanSQL(URLencode(url)))
+  }
  
- #Select unique data from website and turn it to list
+ # Select unique data from website and turn it to list
  
- # playG_ <- sort(ckanUniques("47350364-44a8-4d15-b6e0-5f79ddff9367", "latitude")$latitude)
+
+ playG_name <- sort(ckanUniques("47350364-44a8-4d15-b6e0-5f79ddff9367", "name")$name)
+ playG_neighborhood <- sort(ckanUniques("47350364-44a8-4d15-b6e0-5f79ddff9367", "neighborhood")$neighborhood)
+ playG_ward <- sort(ckanUniques("47350364-44a8-4d15-b6e0-5f79ddff9367", "ward")$ward)
+ interM_policeZ <- sort(ckanUniques("f86f1950-3b73-46f9-8bd4-2991ea99d7c4", "police_zone")$police_zone)
 
  
-### method for cleaning the dataset
- playG_load <- read.csv("playground.csv", header = TRUE)
- playG_load$type <- NA
+ # interM <-  readOGR("https://data.wprdc.org/dataset/31ce085b-87b9-4ffd-adbb-0a9f5b3cf3df/resource/f86f1950-3b73-46f9-8bd4-2991ea99d7c4/download/markingsimg.geojson")
+ # 
  
- playG <- playG_load %>% 
-   mutate(name = as.character(name),
-          maintenance = as.character(maintenance_responsibility),
-          park = as.character(park),
-          neighborhood = as.character(neighborhood),
-          ward = as.numeric(ward),
-          latitude = as.numeric(latitude),
-          longitude = as.numeric(longitude),
-          publicWD = as.numeric(public_works_division))
- waterF_load <- read.csv("water_feature.csv", header = TRUE) 
- waterF <- waterF_load %>%
-   mutate(control_type = as.character(control_type),
-          feater_type = as.character(feature_type))
+ # waterF_load <- read.csv("water_feature.csv", header = TRUE) 
+ # waterF <- waterF_load %>%
+ #   mutate(control_type = as.character(control_type),
+ #          feater_type = as.character(feature_type))
+ 
 
 # Define UI for application 
 ui <- fluidPage(
@@ -81,36 +60,32 @@ ui <- fluidPage(
   #Design sider bar
   sidebarLayout(
     sidebarPanel(
-      # General ledger Date Select
-      # dateRangeInput("DateSelect",
-      #                "date",
-      #                start = Sys.Date()-30,
-      #                end = Sys.Date()),
+    
       selectInput("ParkNameSelect",
                   "park name:",
-                  choices = sort(unique(playG$name)),
+                  choices =  playG_name,
                   multiple = TRUE,
                   selectize = TRUE,
                   selected = c("Arlington Playground","Bon Air Playground","Camp David Lawrence Playground")),
       
       selectInput("NeighborhoodSelect",
                   "neighborhood:",
-                  choices = sort(unique(playG$neighborhood)),
+                  choices = playG_neighborhood,
                   multiple = TRUE,
                   selectize = TRUE,
                   selected = c("South Side Slopes","Brookline","Strip District")),
       
       sliderInput("WardAmountSelect",
                   "ward amount:",
-                  min = min(playG$ward, na.rm = T),
-                  max = max(playG$ward, na.rm = T),
-                  value = c(min(playG$ward, na.rm = T), max(playG$ward, na.rm = T)),
+                  min = min(as.numeric(playG_ward), na.rm = T),
+                  max = max(as.numeric(playG_ward), na.rm = T),
+                  value = c(min(as.numeric(playG_ward), na.rm = T), max(as.numeric(playG_ward), na.rm = T)),
                   step = 1),
       
-      checkboxGroupInput("ControlTypeSelect",
-                         "control type:",
-                         choices = sort(unique(waterF$control_type)),
-                         selected = c("ON/OFF", "Continuous")),
+      checkboxGroupInput("PoliceZoneSelect",
+                         "police zone:",
+                         choices = interM_policeZ,
+                         selected = c("3", "4")),
       
       actionButton("reset", "Reset Filters", icon = icon("refresh"))
     ),
@@ -134,51 +109,64 @@ ui <- fluidPage(
 server <- function(input, output, session = session) {
   # Filtered cost and revenue data
   PGInput <- reactive({
-    playG <- playG 
-   
+    playG <- playG
     
     #Build API Query with proper encodes
     
-    #  url <- paste0("https://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22f61f6e8c-7b93-4df3-9935-4937899901c7%22%20WHERE%20%22general_ledger_date%22%20%3E=%20%27",
-    #                input$DateSelect[1],"%27%20AND%20%22general_ledger_date%22%20%3C=%20%27",input$DateSelect[2],
-    #                "%27%20AND%20%22amount%22%20%3E%3D", input$AmountSelect[1], "%20AND%20%22amount%22%20%3C%3D", input$AmountSelect[2],"%20;")
-    # # 
+
     
     
-    # #Load and clean data
+    #Load and clean data
      # playG <- ckanSQL(url) %>%
      #   mutate(date = as.Date(general_ledger_date))
     
+    #Build API Query with proper encodes
+    #Load and clean data
+    url_1 <- paste0("https://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2247350364-44a8-4d15-b6e0-5f79ddff9367%22") #https://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2247350364-44a8-4d15-b6e0-5f79ddff9367%22
+    ###
+    playG <- ckanSQL(url_1) 
     
     return(playG)
-    
   })
-  WFInput <- reactive({
-    waterF <- waterF
+  
+  IMInput <- reactive({
     
-    return(waterF)
+    url_2<- paste0("https://data.wprdc.org/api/3/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22f86f1950-3b73-46f9-8bd4-2991ea99d7c4%22")
+
+    interM <- ckanSQL(url_2) 
+    
+    return(interM)
   })
   
   # Output Map
-  output$map < renderLeaflet({
+  output$map <- renderLeaflet({
     sp1 <- PGInput()
-    sp2 <- WFInput()
+    sp2 <- IMInput()
+    
     #Call Data and Build Map
     leaflet() %>%
-      #Increat basemaps, the OpenStreet Map and the BalckAndWhite Map, and then create bottoms for choosing between two maps
+      #create basemaps, the OpenStreet Map and the BalckAndWhite Map, and then create bottoms for choosing between two maps
       addTiles(group = "OpenStreetMap.Mapnik(default)") %>%
       addProviderTiles("OpenStreetMap.BlackAndWhite", group = "BW") %>%
       addLayersControl(baseGroups = c("OpenStreetMap.Mapnik(default)", "BW"),
                        options = layersControlOptions(collapsed = FALSE)) %>%
       #Add polygons for Pttsburgh playgrounds
-      addPolygons(data = sp1, color = "#8DD3C7") %>%
+      addMarkers(data = sp1,
+                 lng = ~longitude,
+                 lat = ~latitude) %>%
+      #Add lines for intersction markings in Pittsburgh
+      addPolylines(data = sp2, color = ~ pal5(type)) %>%
+      #Add legends for types of intersection markings
+      addLegend(position = "bottomright" , pal = pal5, values = interM$type, title = "Type") %>%
+      
       #Add points depicting water features in Pittsburgh
-      addMarkers(data = sp2) %>%
+      #addMarkers(data = sp2) %>%
       #Add legends for types of intersection markings
       # addLegend(position = "bottomright" , pal = pal5, values = interM.load$type, title = "Type") %>%
       setView(-80, 40.45, 12)
    })
 
+  
   
   # Three bars are showing the number of the three chosen parks
   output$barplot <- renderPlotly({
@@ -205,7 +193,7 @@ server <- function(input, output, session = session) {
   output$pointsplot <- renderPlotly({
     playG <- PGInput()
     ggplotly(
-      ggplot(data = playG, aes(x = maintenance, y = as.numeric(ward))) + 
+      ggplot(data = playG, aes(x = maintenance_responsibility, y = as.numeric(ward))) + 
         labs(x = "Maintenance Responsibility", y = "Ward Amount", title = "Points Plot for Maintenance Responsibility and Ward Amounts") +
         geom_point())
   })
